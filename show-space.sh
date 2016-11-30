e9f110e5d7dca38ac9c7761133fbf791456f0746
Christopher Nieuwoudt <christophercois@me.com>
2016-11-30 14:42:34 +0200

    Update show-space.sh
    Formating

--- show-space.sh
+++ show-space.sh
@@ -1,3 +1,5 @@
 # Creating initial script
 
-for i in `xe sr-list |grep uuid |awk '{print $5}'`;do echo "$(df -h |grep sr-mount |grep $i |awk '{print $3, "FREE -", $4, "USED"}') - $i -- $(df -h |grep sr-mount |grep $i |awk '{print $1}') $(xe sr-list |grep -A3 $i |grep name-label |cut -f2 -d :)";done |grep USED
+for i in `xe sr-list |grep uuid |awk '{print $5}'`;
+  do echo "$(df -h |grep sr-mount |grep $i |awk '{print $3, "FREE -", $4, "USED"}') - $i -- $(df -h |grep sr-mount |grep $i |awk '{print $1}') $(xe sr-list |grep -A3 $i |grep name-label |cut -f2 -d :)";
+  done |grep USED
